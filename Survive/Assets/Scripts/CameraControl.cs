4d7c4b598e41826a1d5fb83ef665f56ec74f87b9
zach.gharst <zdgharst@gmail.com>
2021-05-09 14:02:42 -0500

    Add third party assets to folder; move folder

---
+++ Survive/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private int speed = 20;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float zoomInput;
+
+    private int horizontalBounds = 80;
+    private int verticalBounds = 100;
+    private int zoomInBound = 7;
+    private int zoomOutBound = 30;
+
+    private float normalizePanning;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        zoomInput = Input.GetAxis("Mouse ScrollWheel") * 7;
+        normalizePanning = transform.position.y / 15;
+
+        /* This seems weird, but it's needed for the z portion of the movement translate to be prevented. */
+        if((transform.position.y == zoomInBound && zoomInput > 0) ||
+           (transform.position.y == zoomOutBound && zoomInput < 0))
+        {
+            zoomInput = 0;
+        }
+
+        Vector3 movement = new Vector3(horizontalInput, -zoomInput, verticalInput + zoomInput / 1.5f);
+        transform.Translate(movement * speed * normalizePanning * Time.deltaTime, Space.World);
+
+        if(Input.GetKeyDown("space"))
+        {
+            transform.position = new Vector3(0, 15, -7);
+        }
+
+
+        if(transform.position.x < -horizontalBounds)
+        {
+            transform.position = new Vector3(-horizontalBounds, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x > horizontalBounds)
+        {
+            transform.position = new Vector3(horizontalBounds, transform.position.y, transform.position.z);
+        }
+
+
+        if(transform.position.y < zoomInBound)
+        {
+            transform.position = new Vector3(transform.position.x, zoomInBound, transform.position.z);
+        }
+        else if(transform.position.y > zoomOutBound)
+        {
+            transform.position = new Vector3(transform.position.x, zoomOutBound, transform.position.z);
+        }
+
+
+        if(transform.position.z < -verticalBounds)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -verticalBounds);
+        }
+        else if(transform.position.z > verticalBounds)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, verticalBounds);
+        }
+    }
+}
---
+++ Survive/Assets/Scripts/Turret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Camera camera;
+    private GameObject head;
+
+    void Start()
+    {
+        camera = Camera.main;
+        head = GameObject.Find("Head");
+    }
+
+    void Update()
+    {
+        // https://docs.unity3d.com/ScriptReference/Camera.ScreenToWorldPoint.html
+        Vector3 input = Input.mousePosition;
+        Vector3 mousePosition = camera.ScreenToWorldPoint(new Vector3(input.x, input.y, camera.transform.position.y));
+        mousePosition.y = 0;
+        head.transform.LookAt(mousePosition + Vector3.up * head.transform.position.y);
+    }
+}
