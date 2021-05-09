dc8058b7a245ee40e036c516379b4931a62125e5
zach.gharst <zdgharst@gmail.com>
2021-05-09 15:51:12 -0500

    Spawn missile at barrel locations

---
+++ Survive/Assets/Scripts/Missile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Transform up, not forward, because of model axis
+        rigidbody.velocity = transform.up * speed;
+        transform.Rotate(new Vector3(0, 1, 0) * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
--- Survive/Assets/Scripts/Turret.cs
+++ Survive/Assets/Scripts/Turret.cs
@@ -7,6 +7,14 @@ public class Turret : MonoBehaviour
     private Camera camera;
     private GameObject head;
 
+    private bool leftBarrel = true;
+    private float barrelXPosLeft = -0.19f;
+    private float barrelXPosRight = 0.25f;
+    private float barrelYPos = 2.44f;
+    private float barrelZPos = 1.60f;
+
+    public GameObject missilePrefab;
+
     void Start()
     {
         camera = Camera.main;
@@ -20,5 +28,23 @@ public class Turret : MonoBehaviour
         Vector3 mousePosition = camera.ScreenToWorldPoint(new Vector3(input.x, input.y, camera.transform.position.y));
         mousePosition.y = 0;
         head.transform.LookAt(mousePosition + Vector3.up * head.transform.position.y);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            float barrelXPos = barrelXPosRight;
+            if(leftBarrel)
+            {
+                barrelXPos = barrelXPosLeft;
+
+            }
+            leftBarrel = !leftBarrel;
+
+            float headDirection = head.transform.rotation.eulerAngles.y + 90;
+            Vector3 headPosition = head.transform.position;
+            Vector3 barrel = head.transform.rotation * new Vector3(barrelXPos, barrelYPos, barrelZPos);
+            Quaternion missileRotation = Quaternion.Euler(0.0f, headDirection, 90.0f);
+
+            Instantiate(missilePrefab, barrel, missileRotation);
+        }
     }
 }
